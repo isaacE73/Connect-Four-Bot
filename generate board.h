512f1fe3215ac15943bb0973de20267af41f7693
isaacE73 <isaacpulido73@gmail.com>
2026-04-17 14:13:56 -0700

    -Refactered  generateboard function. Renamed to makeMove. Now it will update position array with the given move and won't generate the board. -main is just a simulated move for testing perpuses. Not permanante code. -Created Documentaion text file to keep track of notes. It is temperary, it will later be turned into a proper README.

--- generate board.h
+++ generate board.h
@@ -3,7 +3,7 @@
 #include <string>
 using namespace std;
 
-void generateboard() { //initial generation of empty board
+void generateBoard() { //initial generation of empty board
     const string topbottomLine = "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@";
     const string center = "[]          []          []          []          []          []          []          []";
 
@@ -17,43 +17,34 @@ void generateboard() { //initial generation of empty board
         }
     }
     cout << topbottomLine << endl;
-
 }
 
-bool generateboard(int move, bool bot, int gamePosition[6][7]) {
-    const string TOP_LINE = "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@";
-    const string EMPTY_CENTER_LINE = "[]          []";
-    string centerLine;
+bool makeMove(int move, bool which, int gamePosition[7][6]) {
+    if (which) { //if the bot is making a move 
+        for (int i = 5; i >= 0; i--) { //incrament up the column that is being played
+            if (gamePosition[move - 1][i] != 0 && i == 0) return false; //will catch if the whole column is full and return false to siginify an illigal move
+            if (gamePosition[move - 1][i] != 0) continue; //check if the position in the column is taken
 
-    ifstream file;
-
-    file.open("ascii art.txt");
-
-    if (bot) {
-
-
-        for (int i = 5; i >= 0; i--) {
-            if (gamePosition[i][move - 1] != 0 && i == 0) return false;
-            if (gamePosition[i][move - 1] != 0) continue;
-
-            gamePosition[i][move - 1] = 1;
-
+            gamePosition[move - 1][i] = 1; //when an open position is found will set it to 2 representing a player piece
             break;
         }
     }
-    else {
+    else { //if the player is making a move
+        for (int i = 5; i >= 0; i--) { //incrament up the column that is being played
+            if (gamePosition[move - 1][i] != 0 && i == 0) return false; //will catch if the whole column is full and return false to siginify an illigal move
+            if (gamePosition[move - 1][i] != 0) continue; //check if the position in the column is taken
 
+            gamePosition[move - 1][i] = 2; //when an open position is found will set it to 2 representing a player piece
+            break;
+        }
     }
-
-    file.close();
-
-    return false;
+    return true;
 }
 
-void initGamePos(int gamePosition[6][7]) { //initiate gamePos array
+void initGamePos(int gamePosition[7][6]) { //initiate gamePos array
     for (int y = 0; y < 6; y++) { //incroment the y cord
-        for (int x = 0; x < x; x++) { //increment the x cord
-            gamePosition[y][x] = 0; //sets every value to 0 initially
+        for (int x = 0; x < 7; x++) { //increment the x cord
+            gamePosition[x][y] = 0; //sets every value to 0 initially
         }
     }
 }
--- main.cpp
+++ main.cpp
@@ -4,11 +4,31 @@
 using namespace std;
 
 int main() {
-    int gamePosition[6][7];
+    int gamePosition[7][6];
+    const bool bot = true;
+    const bool player = false;
 
     initGamePos(gamePosition);
 
-    
+    for (int y = 0; y < 6; y++) { //incroment the y cord
+        for (int x = 0; x < 7; x++) { //increment the x cord
+            cout << gamePosition[x][y];
+        }
+        cout << endl;
+    }
+
+    gamePosition[6][5] = 2;
+
+    cout << endl;
+
+    makeMove(7, bot, gamePosition);
+
+    for (int y = 0; y < 6; y++) { //incroment the y cord
+        for (int x = 0; x < 7; x++) { //increment the x cord
+            cout << gamePosition[x][y];
+        }
+        cout << endl;
+    }
 
     return 0;
 }
